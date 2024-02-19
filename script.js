89ab3b28c1fe96ca3fa357ca8aa2fec668207eee
Atharva424 <atharvapathare555@gmail.com>
2024-02-19 12:40:07 +0530

    added one index2.html file

---
+++ script.js
@@ -0,0 +1,10 @@
+const arrow = document.querySelector ('.arrow-right');
+const arrown = document.querySelector ('.arrown-right');
+const slider = document.querySelector ('.slider');
+
+
+arrow.addEventListener('click' ,()=>{
+    console.log('Hello');
+})
+
+
